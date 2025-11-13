578406d0d35c02e243b82c52917a2dff468ef1fe
Hakimsb <abdelhakim.sebai@gmail.com>
2025-11-13 14:56:31 -0500

    Q1–Q4 : Création de la classe Livre avec titre, auteur, méthode afficherInfos() et test dans main()

--- ex8.dart
+++ ex8.dart
@@ -1,46 +1,26 @@
-// Q1-Q9 : Classe Livre + héritage avec Roman
+
+
 class Livre {
   String titre;
   String auteur;
-  final int _pages = 200;
-  static int totalLivres = 0;
 
-  Livre(this.titre, this.auteur) {
-    totalLivres++;
-  }
 
-  int get pages => _pages;
+  Livre(this.titre, this.auteur);
+
 
   void afficherInfos() {
-    print("Titre : $titre, Auteur : $auteur, Pages : $pages");
-  }
-
-  static void afficherTotalLivres() {
-    print("Nombre total de livres créés : $totalLivres");
-  }
-}
-
-// Q8-Q9 : Classe Roman héritant de Livre
-class Roman extends Livre {
-  String genre;
-
-  Roman(String titre, String auteur, this.genre) : super(titre, auteur);
-
-  @override
-  void afficherInfos() {
-    print("Titre : $titre, Auteur : $auteur, Genre : $genre, Pages : $pages");
+    print('Titre: $titre, Auteur: $auteur');
   }
 }
 
 void main() {
-  Livre l1 = Livre("Le Petit Prince", "Antoine de Saint-Exupéry");
-  Roman r1 = Roman("Sherlock Holmes", "Arthur Conan Doyle", "Policier");
-  Roman r2 = Roman("Orgueil et Préjugés", "Jane Austen", "Romance");
 
-  l1.afficherInfos();
-  r1.afficherInfos();
-  r2.afficherInfos();
+  var livre1 = Livre('Le Petit Prince', 'Antoine de Saint-Exupéry');
+  var livre2 = Livre('1984', 'George Orwell');
+  var livre3 = Livre('L\'Étranger', 'Albert Camus');
 
-  Livre.afficherTotalLivres();
+
+  livre1.afficherInfos();
+  livre2.afficherInfos();
+  livre3.afficherInfos();
 }
-
