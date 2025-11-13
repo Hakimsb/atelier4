b0ebe8fb372a18eec51ade43a1451a2776abcf99
Hakimsb <abdelhakim.sebai@gmail.com>
2025-11-13 15:03:01 -0500

    Q7 : Ajout de la méthode statique afficherTotalLivres() pour afficher le nombre total de livres

--- ex8.dart
+++ ex8.dart
@@ -3,14 +3,20 @@
 class Livre {
   String titre;
   String auteur;
-
-
+int _pages = 200; 
+ static int totalLivres = 0;
   Livre(this.titre, this.auteur);
 
 
   void afficherInfos() {
     print('Titre: $titre, Auteur: $auteur');
+    totalLivres++;
   }
+  int get pages => _pages;
+   static void afficherTotalLivres() {
+    print('Nombre total de livres créés : $totalLivres');
+  }
+  
 }
 
 void main() {
