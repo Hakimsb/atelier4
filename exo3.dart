684949ae84d150b7afe9bf106f58a1836bb3e528
Hakimsb <abdelhakim.sebai@gmail.com>
2025-11-13 12:14:33 -0500

    Fix : restaurer le contenu complet de exo3.dart pour Q1-Q7

--- exo3.dart
+++ exo3.dart
@@ -1,23 +1,22 @@
 class Compte {
-
   double _solde;
-
   final String numeroCompte;
 
-  
   Compte(this.numeroCompte, [this._solde = 0.0]);
+
   String get solde => "${_solde.toStringAsFixed(2)} \$";
-   void depot(double montant) {
+
+  void depot(double montant) {
     if (montant > 0) {
       _solde += montant;
     } else {
       print("Le montant doit être positif !");
     }
   }
-  void main() {
+}
+
+void main() {
   Compte monCompte = Compte("123456789");
   monCompte.depot(150.0);
   print("Solde du compte ${monCompte.numeroCompte} : ${monCompte.solde}");
-}
-
 }
