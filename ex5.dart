afbbd4093f56789b2e7dea91330a370c1f9b1899
Hakimsb <abdelhakim.sebai@gmail.com>
2025-11-13 13:37:44 -0500

    Q4-Q5 : Création de la classe Cercle avec rayon et implémentation de calculerAire

--- ex5.dart
+++ ex5.dart
@@ -8,3 +8,13 @@ abstract class Forme {
     print("Calcul de l'aire...");
   }
 }
+class Cercle extends Forme {
+  final double rayon;
+
+  Cercle(this.rayon);
+
+  @override
+  double calculerAire() {
+    return 3.14159 * rayon * rayon;
+  }
+}
